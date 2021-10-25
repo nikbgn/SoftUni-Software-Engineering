706b5742cde010af7898684acce2cb28470cac76
Nikolai <pythondevbg8@abv.bg>
2021-10-25 16:47:37 +0300

    Added card Game

--- PROGRAMMING FUNDAMENTALS C# 2021/Lists - Exercise/06. Cards Game/Program.cs
+++ PROGRAMMING FUNDAMENTALS C# 2021/Lists - Exercise/06. Cards Game/Program.cs
@@ -8,7 +8,33 @@ namespace _06._Cards_Game
     {
         static void Main(string[] args)
         {
-            //TODO
+            List<int> hand1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> hand2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (hand1.Count > 0 && hand2.Count > 0)
+            {
+                if (hand1[0] > hand2[0])
+                {
+                    hand1.Add(hand1[0]);
+                    hand1.Add(hand2[0]);
+
+                }
+                else if (hand2[0] > hand1[0])
+                {
+                    hand2.Add(hand2[0]);
+                    hand2.Add(hand1[0]);
+                }
+                hand1.Remove(hand1[0]);
+                hand2.Remove(hand2[0]);
+            }
+
+            if (hand1.Count > hand2.Count)
+            {
+                Console.WriteLine($"First player wins! Sum: {hand1.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {hand2.Sum()}");
+            }
         }
     }
 }
