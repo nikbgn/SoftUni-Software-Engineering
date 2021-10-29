7a6cc5af1845438e1882a901fc958cd4b29a44d4
Nikolai <pythondevbg8@abv.bg>
2021-10-29 18:44:15 +0300

    Add Objects And Classes - Exercise

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phrases = new List<string>
+            {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product."
+            };
+
+            List<string> events = new List<string>
+            {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+
+            List<string> authors = new List<string>
+            {
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"
+            };
+
+            List<string> cities = new List<string>
+            {
+                "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse"
+            };
+
+
+            int numOfFakeReviews = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            for (int i = 0; i < numOfFakeReviews; i++)
+            {
+                string currPhrase = phrases[rand.Next(0, phrases.Count - 1)];
+                string currEvent = events[rand.Next(0, phrases.Count - 1)];
+                string currAuthor = authors[rand.Next(0, phrases.Count - 1)];
+                string currCity = cities[rand.Next(0, phrases.Count - 1)];
+                Console.WriteLine($"{currPhrase} {currEvent} {currAuthor} - {currCity}.");
+            }
+        }
+    }
+}
