c6f32913764d2569a8196e6f01c1133d490afbb4
Nikolai <pythondevbg8@abv.bg>
2021-11-01 18:22:38 +0200

    Add small project Num to binary specific digit count

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Bitwise Operations - Lab/01. Binary Digits Count/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Binary_Digits_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            char binaryDigit = char.Parse(Console.ReadLine());
+            string numBinaryRepresentation = Convert.ToString(num, 2);
+            Console.WriteLine(numBinaryRepresentation.Where(digit => digit == binaryDigit).ToArray().Length);
+        }
+    }
+
+
+    //You are given a positive integer number and one binary digit B (0 or 1). Your task is to write a program that finds the number of binary digits(B) in given integer.
+}
