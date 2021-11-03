f62a107d44b63c7713c6138419a5f146bac93d85
Nikolai <pythondevbg8@abv.bg>
2021-11-03 20:38:12 +0200

    Added Dicts

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+            foreach (int number in numbers)
+            {
+                if (count.ContainsKey(number)) { count[number]++; }
+                else { count.Add(number, 1); }
+            }
+            foreach (var num in count)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+
+        }
+    }
+}
