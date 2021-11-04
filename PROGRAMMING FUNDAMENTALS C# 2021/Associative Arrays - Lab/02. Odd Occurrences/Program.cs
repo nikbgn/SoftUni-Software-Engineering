281d018ca64a68a5cde8b75b859d2d3aa41959bf
Nikolai <pythondevbg8@abv.bg>
2021-11-04 12:57:20 +0200

    Added odd occurances

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> wordsDict = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordsDict.ContainsKey(word))
+                {
+                    wordsDict.Add(word.ToLower(), 1);
+                }
+                else
+                {
+                    wordsDict[word]++;
+                }
+            }
+
+            foreach (var countOfWord in wordsDict)
+            {
+                if(countOfWord.Value % 2 != 0)
+                {
+                    Console.Write(countOfWord.Key + " ");
+                }
+            }
+        }
+    }
+}
