9db2c5eb0ae71402d53127d8369325c872f25b26
Nikolai <pythondevbg8@abv.bg>
2021-11-05 19:13:21 +0200

    Added Miner Task

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string commandKey = Console.ReadLine();
+            while (commandKey != "stop")
+            {
+                int qty = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(commandKey))
+                {
+                    resources.Add(commandKey, 0);
+                }
+
+                resources[commandKey] += qty;
+
+
+                commandKey = Console.ReadLine();
+            }
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
