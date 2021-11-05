8699d584885e5369cb3d76ebe4b4e6e41206529d
Nikolai <pythondevbg8@abv.bg>
2021-11-05 18:43:08 +0200

    Added

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> countOfLetters = new Dictionary<char, int>();
+            foreach (char letter in word)
+            {
+                if (letter != ' ')
+                {
+                    if (countOfLetters.ContainsKey(letter)) { countOfLetters[letter]++; }
+                    else { countOfLetters.Add(letter, 1); }
+                }
+            }
+
+            foreach (var item in countOfLetters)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
