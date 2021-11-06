6bf13f3d473852091baefb9d913ca243c262fed9
Nikolai <pythondevbg8@abv.bg>
2021-11-06 13:04:55 +0200

    Added LINQ exercises

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ Exercise
+
+            List<int> listOfInts = Console.ReadLine().Split().Select(int.Parse).ToList();
+            if (listOfInts.Count < 3)
+            {
+                Console.WriteLine(string.Join(" ",listOfInts));
+            }
+            else
+            {
+                listOfInts = listOfInts.OrderByDescending(x => x).ToList();
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{listOfInts[i]} ");
+                }
+            }
+        }
+    }
+}
---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Lab/05. Word Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ Excercise
+
+            string[] wordsArray = Console.ReadLine().Split().Where(word => word.Length % 2 == 0).ToArray();
+            foreach (string word in wordsArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
