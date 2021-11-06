64a40c84198506c4058eefa73b03aed982233e54
Nikolai <pythondevbg8@abv.bg>
2021-11-06 12:55:45 +0200

    Added Word Synonyms problem

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synonymsDict = new Dictionary<string, List<string>>();
+            int countOfWords = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!synonymsDict.ContainsKey(word))
+                {
+                    synonymsDict.Add(word, new List<string>());
+                }
+                synonymsDict[word].Add(synonym);
+            }
+
+            foreach (var word in synonymsDict)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ",word.Value)}");
+            }
+        }
+    }
+}
