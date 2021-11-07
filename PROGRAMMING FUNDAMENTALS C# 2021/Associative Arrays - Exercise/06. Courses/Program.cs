bc182308d1fe2280289fdd5ba70de89f4d2fa531
Nikolai <pythondevbg8@abv.bg>
2021-11-07 14:26:53 +0200

    Add solution to courses problem

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Exercise/06. Courses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>(); //Course, list of students in the course
+
+            string[] command = Console.ReadLine().Split(" : ");
+            while(command[0] != "end")
+            {
+                string courseName = command[0];
+                string studentName = command[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>() { studentName};
+                }
+                else
+                {
+                    courses[courseName].Add(studentName);
+                }
+
+                command = Console.ReadLine().Split(" : ");
+
+            }
+
+            foreach (var course in courses.OrderByDescending(courseRegisteredUsers => courseRegisteredUsers.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var student in course.Value.OrderBy(name => name))
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+                
+            }
+
+        }
+    }
+}
