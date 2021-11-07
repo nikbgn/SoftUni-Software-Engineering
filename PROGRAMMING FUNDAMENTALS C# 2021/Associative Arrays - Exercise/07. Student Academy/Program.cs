40498703aa161e9819ba2608bca515509ac21e73
Nikolai <pythondevbg8@abv.bg>
2021-11-07 21:22:15 +0200

    Added student academy project

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Associative Arrays - Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfStudents = int.Parse(Console.ReadLine());
+            var students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < numOfStudents; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName,new List<double>() { studentGrade });
+                }
+                else
+                {
+                    students[studentName].Add(studentGrade);
+                }
+                
+            }
+
+
+            foreach (var student in students.OrderByDescending(student => student.Value.Average()).Where(student => student.Value.Average() >= 4.50))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
