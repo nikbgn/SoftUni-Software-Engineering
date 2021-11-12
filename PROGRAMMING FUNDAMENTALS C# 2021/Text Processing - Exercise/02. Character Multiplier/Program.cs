efdb1e02bebbb11fffddbab6a70035f759ec1ee4
Nikolai <pythondevbg8@abv.bg>
2021-11-12 19:38:02 +0200

    More exercises & solutions added

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            int finalSum = GetStringSum(input[0], input[1]);
+            Console.WriteLine(finalSum);
+        }
+
+        private static int GetStringSum(string v1, string v2)
+        {
+            int sum = 0;
+            int minLength = Math.Min(v1.Length,v2.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                sum += v1[i] * v2[i];
+            }
+            string longestLengthString = v1;
+            if (longestLengthString.Length < v2.Length)
+            {
+                longestLengthString = v2;
+            }
+            for (int i = minLength; i < longestLengthString.Length; i++)
+            {
+                sum += longestLengthString[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split("\\");
+            Console.WriteLine($"File name: {path[path.Length-1].Split('.')[0]}\nFile extension: {path[path.Length - 1].Split('.')[1]}");
+        }
+    }
+}
