54fbc724908d7ed07231c25a2addd9b5bd736a72
Nikolai <pythondevbg8@abv.bg>
2021-11-12 19:01:25 +0200

    Added more projects

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sh, too_long_username, !lleg@l ch@rs, jeffbutt
+            string[] userNames = Console.ReadLine().Split(", ");
+            foreach (var userName in userNames)
+            {
+                if (userNameValidator(userName))
+                {
+                    Console.WriteLine(userName);
+                }
+            }
+        }
+
+
+        public static bool userNameValidator(string username)
+        {
+
+            bool lengthCheck = username.Length >= 3 && username.Length <= 16;
+            bool containsIllegalChars = true;
+
+
+            foreach (char character in username)
+            {
+                if (char.IsLetterOrDigit(character) || (character == '_' || character == '-'))
+                {
+                    containsIllegalChars = false;
+                }
+                else
+                {
+                    containsIllegalChars = true;
+                    return false;
+                }
+            }
+
+
+            if (lengthCheck) { return true; }
+            
+
+            return false;
+        }
+    }
+}
