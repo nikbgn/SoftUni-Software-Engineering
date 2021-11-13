b2ea987de86fff844b74114ceac65c07c279d869
Nikolai <pythondevbg8@abv.bg>
2021-11-13 11:45:55 +0200

    Added interesting projects & solutions

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = Console.ReadLine();
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write((char)(test[i]+3));
+            }
+        }
+    }
+}
