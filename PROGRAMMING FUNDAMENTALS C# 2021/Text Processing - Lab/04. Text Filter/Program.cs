3a3db4df9e80c7602f85be1456e00716f7da112c
Nikolai <pythondevbg8@abv.bg>
2021-11-13 10:05:26 +0200

    Added more exercises & solutions

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string lettersText = string.Empty;
+            string digitsText = string.Empty;
+            string otherSymbolsText = string.Empty;
+
+            foreach (var character in text)
+            {
+                if (char.IsLetter(character))
+                {
+                    lettersText += character;
+                }
+                else if (char.IsDigit(character))
+                {
+                    digitsText += character;
+                }
+                else
+                {
+                    otherSymbolsText += character;
+                }
+            }
+
+            Console.WriteLine(digitsText);
+            Console.WriteLine(lettersText);
+            Console.WriteLine(otherSymbolsText);
+
+        }
+    }
+}
