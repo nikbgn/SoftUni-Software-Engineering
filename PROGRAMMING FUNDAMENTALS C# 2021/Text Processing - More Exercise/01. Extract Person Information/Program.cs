c7fd282bd1109ad8ce5214df1afbfbe270fbe67b
Nikolai <pythondevbg8@abv.bg>
2021-11-15 14:44:23 +0200

    More exercises added

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder finalResult = new StringBuilder();
+            int numOfPeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfPeople; i++)
+            {
+                string lineWithInfo = Console.ReadLine();
+                nameAgeExtractor(finalResult,lineWithInfo);
+            }
+
+            Console.WriteLine(finalResult);
+            
+
+        }
+
+        private static void nameAgeExtractor(StringBuilder finalResult, string lineWithInfo)
+        {
+            
+            int nameExtractIndexStart = lineWithInfo.IndexOf('@') + 1;
+            int nameExtractIndexEnd = lineWithInfo.IndexOf('|');
+
+            int ageExtractIndexStart = lineWithInfo.IndexOf('#') + 1;
+            int ageExtractIndexEnd = lineWithInfo.IndexOf('*');
+
+            string exctractedName = lineWithInfo[nameExtractIndexStart..nameExtractIndexEnd];
+            string exctractedAge = lineWithInfo[ageExtractIndexStart..ageExtractIndexEnd];
+
+            finalResult.AppendLine($"{exctractedName} is {exctractedAge} years old.");
+        }
+    }
+}
