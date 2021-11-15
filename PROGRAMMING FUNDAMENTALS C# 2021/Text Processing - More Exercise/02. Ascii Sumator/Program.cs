5958c2d31d7260d991e12234486c48ab2fc2f02c
Nikolai <pythondevbg8@abv.bg>
2021-11-15 14:59:03 +0200

    Added ascii sumator

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string randomString = Console.ReadLine();
+            int asciiSum = 0;
+            foreach (var character in randomString)
+            {
+                if (character > firstChar && character < secondChar) {asciiSum += character; }
+            }
+            Console.WriteLine(asciiSum);
+        }
+    }
+}
