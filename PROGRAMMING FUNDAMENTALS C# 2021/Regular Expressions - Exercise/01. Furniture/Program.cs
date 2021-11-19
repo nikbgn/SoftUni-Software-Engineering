0075b5802c0e4f66304546c6f12bf6d68a693cf3
Nikolai <pythondevbg8@abv.bg>
2021-11-19 19:17:02 +0200

    adde

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneySpentTotal = 0;
+            List<string> boughtFurniture = new List<string>();
+            string regex = @">>(?<item>[A-z][a-z]+|[A-z][A-Z])<<(?<price>\d+\.?\d{2})!(?<qty>\d+)$";
+            string input = Console.ReadLine();
+            while (input != "Purchase")
+            {
+                Match item = Regex.Match(input, regex, RegexOptions.IgnoreCase);
+                if (item.Success)
+                {
+                    boughtFurniture.Add(item.Groups["item"].Value);
+
+                    moneySpentTotal += double.Parse(item.Groups["price"].Value) * double.Parse(item.Groups["qty"].Value);
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+            if (boughtFurniture.Count > 0)
+            {
+                Console.WriteLine(string.Join("\n", boughtFurniture));
+            }
+            Console.WriteLine($"Total money spend: {moneySpentTotal:f2}");
+
+
+        }
+    }
+}
