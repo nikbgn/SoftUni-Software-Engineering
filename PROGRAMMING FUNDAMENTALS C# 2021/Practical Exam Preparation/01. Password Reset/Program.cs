f54fd066dd55af8481e6676edb465e2ed9d9d122
Nikolai <pythondevbg8@abv.bg>
2021-11-28 16:35:25 +0200

    Added More Practice Problems

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Practical Exam Preparation/01. Password Reset/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder output = new StringBuilder(Console.ReadLine());
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "Done")
+            {
+                string currCmd = command[0];
+
+                switch (currCmd)
+                {
+                    case "TakeOdd":
+                        takeOdd(output);
+                        Console.WriteLine(output);
+                        break;
+                    case "Cut":
+                        int startIndexCut = int.Parse(command[1]);
+                        int lenOfCut = int.Parse(command[2]);
+                        output.Remove(startIndexCut, lenOfCut);
+                        Console.WriteLine(output);
+                        break;
+                    case "Substitute":
+                        string substituteSubstring = command[1];
+                        string substituteWith = command[2];
+                        if (output.ToString().Contains(substituteSubstring))
+                        {
+                            output.Replace(substituteSubstring, substituteWith);
+                            Console.WriteLine(output);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine($"Your password is: {output}");
+        }
+
+        private static void takeOdd(StringBuilder output)
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+
+                    temp.Append(output[i]);
+                }
+            }
+            output.Clear();
+            output.Append(temp);
+        }
+    }
+}
---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Practical Exam Preparation/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countOfBarcodes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfBarcodes; i++)
+            {
+                string input = Console.ReadLine();
+                string regex = @"@#+(?<barcode>[A-Z]+[A-Za-z0-9]+[A-Z]+)@#+";
+                MatchCollection matches = Regex.Matches(input, regex);
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                    {
+                        if (match.Groups["barcode"].Value.Length > 5)
+                        {
+                            groupDecide(match.Groups["barcode"].Value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid barcode");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+
+        }
+
+        private static void groupDecide(string value)
+        {
+
+            string group = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsDigit(value[i]))
+                {
+                    group += value[i];
+                }
+            }
+            if (group.Length > 0) Console.WriteLine($"Product group: {group}");
+            else Console.WriteLine("Product group: 00");
+
+
+        }
+    }
+}
