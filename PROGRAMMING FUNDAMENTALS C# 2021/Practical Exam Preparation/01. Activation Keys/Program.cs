7b253d037c58e5aa32b1ce61fea604b717aff28f
Nikolai <pythondevbg8@abv.bg>
2021-11-30 08:17:28 +0200

    Preparation exercise added

---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Practical Exam Preparation/01. Activation Keys/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace _01._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawKey = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(rawKey);
+            string[] command = Console.ReadLine().Split(">>>");
+            while (command[0] != "Generate")
+            {
+                string currentCommand = command[0];
+                switch (currentCommand)
+                {
+                    case "Contains":
+                        string substring = command[1];
+                        if (sb.ToString().Contains(substring)) Console.WriteLine($"{sb} contains {substring}");
+                        else Console.WriteLine("Substring not found!");
+                        break;
+                    case "Flip":
+                        string flipTo = command[1]; //Can contain Upper or Lower
+                        int startIndex = int.Parse(command[2]);
+                        int endIndex = int.Parse(command[3]);
+                        int len = Math.Abs(endIndex - startIndex);
+                        if (flipTo == "Upper")
+                        {
+                            sb.Replace(sb.ToString().Substring(startIndex, len), sb.ToString().Substring(startIndex, len).ToUpper());
+                            Console.WriteLine(sb);
+                        }
+                        else
+                        {
+                            sb.Replace(sb.ToString().Substring(startIndex, len), sb.ToString().Substring(startIndex, len).ToLower());
+                            Console.WriteLine(sb);
+                        }
+
+                        break;
+                    case "Slice":
+                        int startSlice = int.Parse(command[1]);
+                        int endSlice = int.Parse(command[2]);
+                        int lenSlice = Math.Abs(endSlice - startSlice);
+                        sb.Remove(startSlice, lenSlice);
+                        Console.WriteLine(sb);
+                        break;
+                    default:
+                        break;
+                }
+                command = Console.ReadLine().Split(">>>");
+            }
+
+            Console.WriteLine($"Your activation key is: {sb}");
+        }
+    }
+}
---
+++ PROGRAMMING FUNDAMENTALS C# 2021/Practical Exam Preparation/02. Emoji Detector/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ulong threshold = findThreshold(input,false);
+            Console.WriteLine($"Cool threshold: {threshold}");
+            string pattern = @"(?<emojiWithSymbols>(\:{2}|\*{2})(?<emojiName>[A-Z][a-z]{2,})\1)";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int emojiCount = 0;
+            List<String> emojiList = new List<string>();
+            foreach (Match match in matches)
+            {
+                emojiCount++;
+                string currEmojiName = match.Groups["emojiName"].Value;
+                string currEmojiWithSymbols = match.Groups["emojiWithSymbols"].Value;
+                ulong currEmojiTreshold = findThreshold(currEmojiName,true);
+                if (currEmojiTreshold > threshold)
+                {
+                    if (emojiList.Contains(currEmojiWithSymbols) == false)
+                    {
+                        emojiList.Add(currEmojiWithSymbols);
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"{emojiCount} emojis found in the text. The cool ones are:");
+
+            foreach (var item in emojiList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        public static ulong findThreshold(string input, bool flagEmojiOrNo)
+        {
+            ulong result = 1;
+
+            if(flagEmojiOrNo == false) //Calculate treshold of input
+            {
+                foreach (char elem in input)
+                {
+                    if (char.IsDigit(elem)) { result *= ulong.Parse(elem.ToString()); }
+                }
+            }
+            else //Else, the flagEmojiOrNo = true -> calculate emoji treshold
+            {
+                foreach (char elem in input)
+                {
+                    result += elem;
+                }
+            }
+
+            return result;
+        }
+    }
+}
