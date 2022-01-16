041f3f186f6c4e7be363f336ca6def347ea10f49
Nikolai <pythondevbg8@abv.bg>
2022-01-16 16:58:32 +0200

    add

---
+++ ADVANCED C# 2022/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+            //Fill in the matrix.
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currRowNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRowNums[col];
+                }
+            }
+
+            int leftDiagonal = GetDiagonal(matrix, 'l');
+            int rightDiagonal = GetDiagonal(matrix, 'r');
+            Console.WriteLine(Math.Abs(leftDiagonal-rightDiagonal));
+
+
+        }
+
+        private static int GetDiagonal(int[,] matrix, char diagonalFlag)
+        {
+            int sum = 0;
+            if (diagonalFlag == 'l')
+            {
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sum += matrix[i, i];
+                }
+            }
+            else if (diagonalFlag == 'r')
+            {
+                int startC = matrix.GetLength(0) - 1;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, startC];
+                    startC--;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ ADVANCED C# 2022/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] matrixSizes = Console.ReadLine().Split();
+            int rowSize = int.Parse(matrixSizes[0]);
+            int colSize = int.Parse(matrixSizes[1]);
+
+            char[,] matrix = new char[rowSize, colSize];
+
+            int found2x2SquaresCount = 0;
+
+            for (int row = 0; row < rowSize; row++)
+            {
+                var currentLineChars = Console.ReadLine().Split();
+                for (int col = 0; col < colSize; col++)
+                {
+                    matrix[row, col] = char.Parse(currentLineChars[col]);
+                }
+            }
+
+
+            
+            
+            for (int row = 0; row < rowSize-1; row++)
+            {
+                for (int col = 0; col < colSize-1; col++)
+                {
+                    int charStart = matrix[row, col];
+                    if(matrix[row,col] == charStart && matrix[row,col+1] == charStart && matrix[row+1,col] == charStart && matrix[row + 1, col + 1] == charStart)
+                    {
+                        found2x2SquaresCount++;
+                    }
+                }
+            }
+            Console.WriteLine(found2x2SquaresCount);
+        }
+    }
+}
