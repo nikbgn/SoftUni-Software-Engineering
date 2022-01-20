376c3ebe1ee552bcca3dad3ab7486f74565ea1a7
Nikolai <pythondevbg8@abv.bg>
2022-01-20 16:02:18 +0200

    Sets&Dictionaries - Exercises added

---
+++ ADVANCED C# 2022/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> chemicals = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in currLine)
+                {
+                    chemicals.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals.OrderBy(ascending => ascending)));
+        }
+    }
+}
