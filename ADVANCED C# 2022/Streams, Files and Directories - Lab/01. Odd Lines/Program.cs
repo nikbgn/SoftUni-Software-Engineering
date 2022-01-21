3e1498addaa847a70073ba4e36796e5643b0b34a
Nikolai <pythondevbg8@abv.bg>
2022-01-21 21:52:16 +0200

    adding streams files and dictionaries

---
+++ ADVANCED C# 2022/Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+
+namespace OddLines { 
+public class OddLines
+{
+    static void Main()
+    {
+        string inputFilePath = @"..\..\..\input.txt";
+        string outputFilePath = @"..\..\..\output.txt";
+        ExtractOddLines(inputFilePath, outputFilePath);
+    }
+    public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+    {
+        var reader = new StreamReader(inputFilePath);
+        int index = 0;
+        using (reader)
+        {
+
+            string line = reader.ReadLine();
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                while (line != null)
+                {
+                    index++;
+                    if (index % 2 == 1)
+                    {
+                        writer.WriteLine(line);
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+
+        }
+    }
+}
+
+}
+
---
+++ ADVANCED C# 2022/Streams, Files and Directories - Lab/Streams, Files and Directories - Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Streams__Files_and_Directories___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
