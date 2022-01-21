6062b219960f489ac022aece99c650677a8259e2
Nikolai <pythondevbg8@abv.bg>
2022-01-21 23:09:17 +0200

    added Line Numbers

---
+++ ADVANCED C# 2022/Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                
+                int linecount = 1;
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    var line = reader.ReadLine();
+                    while(line != null)
+                    {
+                        writer.WriteLine($"{linecount}. {line}");
+                        linecount++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
