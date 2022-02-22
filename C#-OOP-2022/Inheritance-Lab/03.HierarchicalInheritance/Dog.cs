7f3e8240d55823c1e1bdac89dd070ea69998f1b3
Nikolai <pythondevbg8@abv.bg>
2022-02-22 21:13:37 +0200

    added

---
+++ C#-OOP-2022/Inheritance-Lab/03.HierarchicalInheritance/Dog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark() => Console.WriteLine("barking...");
+    }
+}
---
+++ C#-OOP-2022/Inheritance-Lab/05.StackofStrings/StackOfStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack<string> AddRange(params string[] items) => new Stack<string>(items);
+    }
+}
