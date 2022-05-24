3c4a6ecd72acc7af7b142b488d29a7ecef7b748e
Nikolai <pythondevbg8@abv.bg>
2022-05-24 13:42:56 +0300

    add

---
+++ CSharp-DB/DB-Basics/CRUD/T21IncreaseSalaries.sql
@@ -0,0 +1,11 @@
+UPDATE [Employees]
+SET [Salary] += [Salary] * 0.12
+ WHERE [DepartmentID] 
+    IN 
+ (SELECT
+		[DepartmentID]
+  FROM  [Departments]
+ WHERE  [Name] IN ('Engineering', 'Tool Design', 'Marketing','Information Services')
+ )
+
+SELECT [Salary] FROM [Employees]
