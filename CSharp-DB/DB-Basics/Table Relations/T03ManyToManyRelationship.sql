9003772d3bd31aa8c04c4a51e816a2fc38ae051a
Nikolai <pythondevbg8@abv.bg>
2022-05-30 10:34:46 +0300

    add

---
+++ CSharp-DB/DB-Basics/Table Relations/T03ManyToManyRelationship.sql
@@ -0,0 +1,19 @@
+CREATE TABLE [Students]
+(
+	[StudentID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30) NOT NULL
+)
+
+CREATE TABLE [Exams]
+(
+	[ExamID] INT PRIMARY KEY IDENTITY(101,1),
+	[Name] NVARCHAR(80) NOT NULL
+)
+
+CREATE TABLE [StudentsExams]
+(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]),
+	[ExamID] INT FOREIGN KEY REFERENCES [Exams]([ExamID]),
+	PRIMARY KEY ([StudentID],[ExamID])
+)
+
