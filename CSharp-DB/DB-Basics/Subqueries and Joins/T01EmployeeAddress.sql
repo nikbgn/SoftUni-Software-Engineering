2af938eccff92bd85631526478258f60470e2727
Nikolai <pythondevbg8@abv.bg>
2022-06-02 18:25:55 +0300

    add

--- CSharp-DB/DB-Basics/Subqueries and Joins/T01EmployeeAddress.sql
+++ CSharp-DB/DB-Basics/Subqueries and Joins/T01EmployeeAddress.sql
@@ -1,4 +1,6 @@
-SELECT [EmployeeID], [JobTitle], [e].[AddressID]
+SELECT TOP(5) [EmployeeID], [JobTitle], [e].[AddressID], [a].[AddressText]
   FROM [Employees] AS [e]
 LEFT JOIN [Addresses] AS [a]
 	ON [e].[AddressID] = [a].[AddressID]
+ ORDER 
+    BY [a].[AddressID]
