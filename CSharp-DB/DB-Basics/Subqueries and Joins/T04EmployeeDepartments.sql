85a8b6ae73eff6826881960ebb68ffd5ce982887
Nikolai <pythondevbg8@abv.bg>
2022-06-03 13:46:04 +0300

    correct code upload

--- CSharp-DB/DB-Basics/Subqueries and Joins/T04EmployeeDepartments.sql
+++ CSharp-DB/DB-Basics/Subqueries and Joins/T04EmployeeDepartments.sql
@@ -1,4 +1,9 @@
-SELECT e.EmployeeID, e.FirstName, e.Salary, d.[Name] AS [DepartmentName]
+
+SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.[Name] AS [DepartmentName]
   FROM [Employees] AS e
+  LEFT
   JOIN [Departments] AS d
-  
+    ON e.DepartmentID = d.DepartmentID
+ WHERE e.Salary > 15000
+ ORDER
+    BY e.DepartmentID
