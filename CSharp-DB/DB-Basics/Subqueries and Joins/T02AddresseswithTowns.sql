7e5f8f8565c9ae17ce65acba2b6d38cd262cf89e
Nikolai <pythondevbg8@abv.bg>
2022-06-03 13:37:04 +0300

    add

--- CSharp-DB/DB-Basics/Subqueries and Joins/T02AddresseswithTowns.sql
+++ CSharp-DB/DB-Basics/Subqueries and Joins/T02AddresseswithTowns.sql
@@ -1,5 +1,5 @@
 
-SELECT [e].[FirstName],[e].[LastName],[t].[Name] AS [Town], [a].[AddressText]
+SELECT TOP(50) [e].[FirstName],[e].[LastName],[t].[Name] AS [Town], [a].[AddressText]
 FROM [Employees] AS [e]
 JOIN [Addresses] AS [a]
   ON [a].AddressID = [e].[AddressID]
