cdd70d8ff4221a2182027c69304bf8d2bebd82bb
Nikolai <pythondevbg8@abv.bg>
2022-07-03 18:08:03 +0300

    add

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T07EmployeesandProjects/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesInPeriod(context));
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.EmployeesProjects.Any(ep => ep.Project.StartDate.Year >= 2001 && ep.Project.StartDate.Year <= 2003))
+                .Take(10)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    AllProjects = e.EmployeesProjects.Select(p => new
+                    {
+                        ProjectName = p.Project.Name,
+                        StartDate = p.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt"),
+                        EndDate = p.Project.EndDate.HasValue ?
+                        p.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished"
+                    }).ToArray()
+                }).ToArray();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} - Manager: {emp.ManagerFirstName} {emp.ManagerLastName}");
+                foreach (var proj in emp.AllProjects)
+                {
+                    sb.AppendLine($"--{proj.ProjectName} - {proj.StartDate} - {proj.EndDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+
+
+        }
+    }
+}
