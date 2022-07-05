ecd4f38abbb892dd171ee8ea4f8f0fcdb311d479
Nikolai <pythondevbg8@abv.bg>
2022-07-05 11:32:01 +0300

    add

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T08AddressesbyTown/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetAddressesByTown(context));
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            var addresses = context.Addresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town.Name)
+                .ThenBy(a => a.AddressText)
+                .Select(a => new
+                {
+                    a.Employees,
+                    TownName = a.Town.Name,
+                    a.AddressText
+                })
+                .Take(10)
+                .ToArray();
+
+
+            foreach (var item in addresses)
+            {
+                sb.AppendLine($"{item.AddressText}, {item.TownName} - {item.Employees.Count} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
