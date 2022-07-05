be919347efb4127e81449f2104a8770b8d745bc0
Nikolai <pythondevbg8@abv.bg>
2022-07-05 13:55:43 +0300

    T14 Added

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T14DeleteProjectbyId/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(DeleteProjectById(context));
+        }
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var toDelete = context
+                .Projects
+                .Where(p => p.ProjectId == 2)
+                .First();
+
+            var employeesProjects = context
+                .EmployeesProjects
+                .Where(p => p.ProjectId == 2);
+
+            foreach (var project in employeesProjects)
+            {
+                context.EmployeesProjects.Remove(project);
+            }
+
+            context.Projects.Remove(toDelete);
+            context.SaveChanges();
+
+            var projects = context
+                .Projects
+                .Select(p => p.Name)
+                .Take(10)
+                .ToArray();
+
+            foreach (var proj in projects)
+            {
+                sb.AppendLine(proj);
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
