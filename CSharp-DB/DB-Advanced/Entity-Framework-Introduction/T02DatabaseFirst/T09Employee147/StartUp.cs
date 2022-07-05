5d5fc0547d21b6bd09af66a0989fa09c34a62d96
Nikolai <pythondevbg8@abv.bg>
2022-07-05 12:23:55 +0300

    add

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T09Employee147/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployee147(context));
+        }
+        public static string GetEmployee147(SoftUniContext context)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+
+            var employee = context.Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects
+                        .Select(p => p.Project.Name)
+                        .OrderBy(p => p)
+                        .ToArray()
+                })
+                .FirstOrDefault();
+
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+            foreach (var project in employee.Projects)
+            {
+                sb.AppendLine(project);
+            }
+
+
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
