dd851b408c9aaef9829e59a39dc4683cef9819a9
Nikolai <pythondevbg8@abv.bg>
2022-07-05 13:24:00 +0300

    add

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T10DepartsMoreThan5Employees/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(context));
+
+        }
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var departments = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .Select(d => new
+                {
+                    DeparmentName = d.Name,
+                    ManagerFirstName = d.Manager.FirstName,
+                    ManagerLastName = d.Manager.LastName,
+                    Employees = d.Employees.Select(e => new
+                    {
+                        EmployeeFirstName = e.FirstName,
+                        EmployeeLastName = e.LastName,
+                        EmployeeJobTitle = e.JobTitle
+                    })
+                    .OrderBy(e => e.EmployeeFirstName)
+                    .ThenBy(e => e.EmployeeLastName)
+                    .ToArray()
+                })
+                .ToArray();
+
+            foreach (var department in departments)
+            {
+                sb.AppendLine($"{department.DeparmentName} - {department.ManagerFirstName} {department.ManagerLastName}");
+                foreach (var employee in department.Employees)
+                {
+                    sb.AppendLine($"{employee.EmployeeFirstName} {employee.EmployeeLastName} - {employee.EmployeeJobTitle}");
+                }
+            }
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T11FindLatest10Projects/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetLatestProjects(context));
+        }
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var lastestProjects = context
+                .Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .Select(p => new
+                {
+                    ProjectName = p.Name,
+                    ProjectDescription = p.Description,
+                    ProjectStartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt")
+                })
+                .OrderBy(p => p.ProjectName)
+                .ToArray();
+
+            foreach (var project in lastestProjects)
+            {
+                sb.AppendLine(project.ProjectName);
+                sb.AppendLine(project.ProjectDescription);
+                sb.AppendLine(project.ProjectStartDate);
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
