cd8af637c02b7b2ffbf9ef113b7f9a8d6e517204
Nikolai <pythondevbg8@abv.bg>
2022-07-05 13:41:36 +0300

    add

---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T12IncreaseSalaries/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(IncreaseSalaries(context));
+        }
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(
+                    e => e.Department.Name == "Engineering" ||
+                         e.Department.Name == "Tool Design" ||
+                         e.Department.Name == "Marketing" ||
+                         e.Department.Name == "Information Services");
+
+            foreach (var emp in employees)
+            {
+                emp.Salary += emp.Salary * 0.12M;
+            }
+
+            context.SaveChanges();
+
+            var employeesUpdated = context
+                .Employees
+                .Where(
+                    e => e.Department.Name == "Engineering" ||
+                         e.Department.Name == "Tool Design" ||
+                         e.Department.Name == "Marketing" ||
+                         e.Department.Name == "Information Services")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToArray();
+
+
+            foreach (var emp in employeesUpdated)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} (${emp.Salary:f2})");
+            }
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-DB/DB-Advanced/Entity-Framework-Introduction/T02DatabaseFirst/T13.FindEmployeesByFirstName/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesByFirstNameStartingWithSa(context));
+        }
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.FirstName.ToLower().StartsWith("sa"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .OrderBy(e=>e.FirstName)
+                .ThenBy(e=>e.LastName)
+                .ToArray();
+
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} - {emp.JobTitle} - (${emp.Salary:f2})");
+            }
+
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
