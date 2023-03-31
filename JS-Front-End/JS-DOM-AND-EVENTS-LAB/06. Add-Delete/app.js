87acdc992702a091d9bba52403f18a388739b441
Nikolai <pythondevbg8@abv.bg>
2023-03-31 15:48:42 +0300

    Add-Delete Task

--- JS-Front-End/JS-DOM-AND-EVENTS-LAB/06. Add-Delete/app.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-LAB/06. Add-Delete/app.js
@@ -1,3 +1,15 @@
 function addItem() {
-    //TODO...
+  let listOfItems = document.getElementById("items");
+  let inputBox = document.getElementById("newItemText");
+  let newItem = document.createElement("li");
+  let deleteOption = document.createElement("a");
+  newItem.textContent = inputBox.value;
+  deleteOption.textContent = "[Delete]";
+  deleteOption.setAttribute("href", "#");
+  deleteOption.addEventListener("click", function (e) {
+    this.parentElement.remove();
+  });
+  newItem.appendChild(deleteOption);
+  listOfItems.appendChild(newItem);
+  inputBox.value = "";
 }
