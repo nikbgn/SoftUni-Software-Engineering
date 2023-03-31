8a2297c1499ab8a02aca9b7f4e6e90403bb1a2c9
Nikolai <pythondevbg8@abv.bg>
2023-03-31 16:04:01 +0300

    07-Colorize-Table Task

--- JS-Front-End/JS-DOM-AND-EVENTS-LAB/07. Colorized-Table/ColorizeTable.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-LAB/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+  let idx = 0;
+  let trElements = Array.from(document.getElementsByTagName("tr"));
+  trElements.forEach((currentRow) => {
+    if (idx % 2 !== 0) {
+      currentRow.style.background = "teal";
+    }
+    idx++;
+  });
 }
