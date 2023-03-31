d09e17ff457dcaa198eb687a55e061f7996dd5a7
Nikolai <pythondevbg8@abv.bg>
2023-03-31 16:12:02 +0300

    08-SumTable Task

--- JS-Front-End/JS-DOM-AND-EVENTS-LAB/08. Sum-Table/SumTable.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-LAB/08. Sum-Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
-
+  let trElements = Array.from(document.getElementsByTagName("tr"));
+  let finalPriceTr = trElements[trElements.length - 1];
+  trElements = trElements.slice(1, trElements.length - 1);
+  let sum = 0;
+  trElements.forEach((product) => {
+    let productPrice = Number(product.children[1].textContent);
+    sum += productPrice;
+  });
+  finalPriceTr.children[1].textContent = sum;
 }
