418d1cc866a9289f55fc2d7c5aa06523a2a86e92
Nikolai <pythondevbg8@abv.bg>
2023-04-12 20:26:20 +0300

    Initial commit

---
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    const firstNumber = Number(document.getElementById('firstNumber').value);
+    const secondNumber = Number(document.getElementById('secondNumber').value);
+
+    const result = document.getElementById('result');
+    result.textContent = firstNumber - secondNumber;
+}
