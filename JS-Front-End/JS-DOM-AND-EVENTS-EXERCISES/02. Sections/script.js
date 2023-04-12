24b37d321d396f74fba713beb2f658660f4ab8bf
Nikolai <pythondevbg8@abv.bg>
2023-04-12 20:50:18 +0300

    Tasks

--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/02. Sections/script.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/02. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+  let contentDiv = document.getElementById("content");
+  words.forEach((section) => {
+    const sectionDiv = document.createElement("div");
+    const sectionParagraph = document.createElement("p");
+    sectionParagraph.textContent = section;
+    sectionParagraph.style.display = "none";
+    sectionDiv.appendChild(sectionParagraph);
+    sectionDiv.addEventListener("click", () => {
+      sectionParagraph.style.display = "block";
+    });
+    
+    contentDiv.appendChild(sectionDiv);
+  });
 }
--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/03. Accordion/accordion.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+  const button = document.getElementsByClassName("button")[0];
+  const information = document.getElementById("extra");
+
+  if (button.textContent.toLowerCase() === "more") {
+    information.style.display = "block";
+    button.textContent = "Less";
+  } else {
+    information.style.display = "none";
+    button.textContent = "More";
+  }
 }
