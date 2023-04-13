5f23035b2df4a5e67c6376e989bddb3349838f6e
Nikolai <pythondevbg8@abv.bg>
2023-04-13 16:31:03 +0300

    Dropdown task

--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/05. Fill-Dropdown/dropdown.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+  const newItemText = document.getElementById("newItemText");
+  const newItemValue = document.getElementById("newItemValue");
+  const select = document.getElementById("menu");
+  const newOption = document.createElement("option");
+  newOption.textContent = newItemText.value;
+  newOption.value = newItemValue.value;
+  select.appendChild(newOption);
+  newItemText.value = "";
+  newItemValue.value = "";
 }
