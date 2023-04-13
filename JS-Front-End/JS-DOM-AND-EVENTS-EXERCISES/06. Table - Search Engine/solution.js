074e7d7e5c6fddbe04775b91229d730d58db9ab8
Nikolai <pythondevbg8@abv.bg>
2023-04-13 16:38:14 +0300

    Search Engine Task

--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/06. Table - Search Engine/solution.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/06. Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  const tableBody = document.getElementsByTagName("tbody")[0];
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const tBodyChildren = Array.from(tableBody.children);
+    const searchingFor = document.getElementById("searchField");
+    tBodyChildren.forEach((child) => {
+      child.classList.remove("select");
+      let rowText = child.innerText.trim();
+      if (rowText.includes(searchingFor.value)) {
+        child.classList.add("select");
+      }
+    });
 
-   }
+    searchingFor.value = "";
+  }
 }
