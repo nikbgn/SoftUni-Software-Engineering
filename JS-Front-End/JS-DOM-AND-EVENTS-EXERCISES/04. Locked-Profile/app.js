fe39a71ab23b5c699321eed2a39bacbd695e693a
Nikolai <pythondevbg8@abv.bg>
2023-04-13 16:23:36 +0300

    task

--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/04. Locked-Profile/app.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/04. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const profiles = Array.from(document.querySelectorAll(".profile"));
+  profiles.forEach((profile) => {
+    //Add event listener to the profile's button.
+    const button = profile.querySelector("button");
+    button.addEventListener("click", clickHandler);
+  });
+
+  function clickHandler() {
+    const button = this;
+    const currentProfile = this.parentElement;
+    const divWithInfo = currentProfile.querySelector("div");
+    const unlock = currentProfile.querySelector('input[value="unlock"]');
+
+    if (unlock.checked) {
+      if (button.textContent === "Show more") {
+        divWithInfo.style.display = "block";
+        button.textContent = "Hide it";
+      } else {
+        divWithInfo.style.display = "none";
+        button.textContent = "Show more";
+      }
+    }
+  }
 }
