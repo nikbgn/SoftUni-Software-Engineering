507402f898718761ebc5ee1e509b15395c1ad177
Nikolai <pythondevbg8@abv.bg>
2023-04-13 20:17:34 +0300

    Format text task

--- JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/07. Format the Text/solution.js
+++ JS-Front-End/JS-DOM-AND-EVENTS-EXERCISES/07. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  const textArea = document.getElementById("input");
+  const output = document.getElementById("output");
+  let sentences = Array.from(
+    textArea.value.split(".").map((sentence) => sentence.trim())
+  );
+
+  sentences.pop();
+
+  while (sentences.length > 0) {
+    let newParagraph = document.createElement("p");
+    let getSentences = sentences.slice(0, 3);
+    newParagraph.textContent = getSentences.join(".") + ".";
+    for (let i = 0; i < 3; i++) {
+      sentences.shift();
+    }
+
+    output.appendChild(newParagraph);
+  }
 }
